d5484da264f8e01a235d0c7783264a65a4a38e30
fokinko <stolcp@gmail.com>
2016-06-20 09:45:33 +0200

    initial commit v1.0.1

--- models/users.js
+++ models/users.js
@@ -1,5 +1,6 @@
 exports.id = 'users';
 var users = [];
+var usersObj = {};
 
 exports.init = function(self) {
     OPENPLATFORM.getUsers(self.openplatform, self.id, function(err, response) {
@@ -7,10 +8,18 @@ exports.init = function(self) {
             console.log("Error getting users -> ", err);
         } else {
             users = response;
+            for (var i = 0; i < response.length; i++){
+                usersObj[response[i].id] = response[i];
+            }
         }
     });
+
 };
 
 exports.getUsers = function(){
     return users;
+}
+
+exports.getUsersObj = function(){
+    return usersObj;
 }
